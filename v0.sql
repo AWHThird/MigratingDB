538a24289d79a6bfca4e9b82b0cecdd7da842203
Arlist W Hunter III <awhthird@gmail.com>
2018-10-13 16:38:30 -0400

    defined dbo.employee

--- v0.sql
+++ v0.sql
@@ -20,3 +20,27 @@ GO
 --Use Database
 USE MigratingDB;
 GO
+
+--Drop Table dbo.Employee if exists
+DROP TABLE IF EXISTS dbo.[Employee];
+GO
+
+--Create Table dbo.[Employee] with identity
+CREATE TABLE dbo.[Employee] (
+  [EmployeeId] INT IDENTITY(1,1) NOT NULL
+  ,[EmployeeName] VARCHAR(256)
+  ,[DepartmentName] VARCHAR(256)
+  ,[JobTitle] VARCHAR(256)
+  ,[HomeAddress] VARCHAR(256)
+  ,[HomePhone] VARCHAR(32)
+  ,[CellPhone] VARCHAR(32)
+  ,[StartDate] DateTime2(0)
+  ,[EndDate] DateTime2(0)
+  ,[Employed] BIT
+);
+GO
+
+--Create Primary Key PK_Employee_EmployeeId PRIMARY KEY CLUSTERED
+ALTER TABLE dbo.[Employee]
+ADD CONSTRAINT PK_Employee_EmployeeId PRIMARY KEY CLUSTERED (EmployeeId);
+GO
